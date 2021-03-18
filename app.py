3f597af74353c940fcad3e3fc68b3ae45661c764
Semyon <shudss00@gmail.com>
2021-03-18 19:03:41 +0400

    Add getImageHandler

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from aiohttp import web
+import base64
+import aiohttp_cors
 
-from application.Router import Router
+
+def staticHandler(request):
+    return web.FileResponse('./public/index.html')
+
+
+async def getImageHandler(request):
+    post = await request.json()
+    images = post.get("images")
+    print(request.content_type)
+    print(images[0]["result"])
+    return web.Response(text="Получил", content_type="text")
+
 
 app = web.Application()
-Router(app, web)
+app.router.add_route("POST", "/api/images", getImageHandler)
 
-web.run_app(app, port=8080)
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            allow_headers="*"
+    )
+})
+
+for route in list(app.router.routes()):
+    cors.add(route)
+
+web.run_app(app, port=9000)
