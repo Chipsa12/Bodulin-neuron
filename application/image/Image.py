7192c4114f3ac116f073abd5362f80d75b646f6a
Semyon <shudss00@gmail.com>
2021-03-26 20:46:46 +0400

    Add neural network

--- application/image/Image.py
+++ application/image/Image.py
@@ -3,18 +3,13 @@ import re
 
 
 class Image:
-    def __init__(self, name, binary, formatImg):
+    def __init__(self, name, binary):
         self.name = name
         self.binary = binary
-        self.formatImg = formatImg
 
     @staticmethod
     def decodeImage(imageName, encryptedImage):
         result = re.split(r",", encryptedImage, maxsplit=1)
         imageFormat = ""
         binaryImage = base64.b64decode(result[1])
-        if "image/jpeg" in result[0]:
-            imageFormat = "jpeg"
-        elif "image/png" in result[0]:
-            imageFormat = "png"
-        return Image(imageName, binaryImage, imageFormat)
+        return Image(imageName, binaryImage)
