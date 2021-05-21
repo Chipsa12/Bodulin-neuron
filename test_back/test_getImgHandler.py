214f0e6a2747a7e12ef6a40da96987ac64d307cc
AndreyBodulin <andrey_bodulin@mail.ru>
2021-05-22 03:28:52 +0400

    change test

---
+++ test_back/test_getImgHandler.py
@@ -0,0 +1,12 @@
+import pytest
+from PIL import Image, ImageChops
+from app import getImagesHandler
+
+
+
+@pytest.mark.parametrize('request'[
+    (
+    )
+])
+def test_getImagesHandler(images, answer):
+    return
